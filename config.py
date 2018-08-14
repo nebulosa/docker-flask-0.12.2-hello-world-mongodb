662e34f6cdf4673edfa7452239daa7dc1386ed8e
Javier López <m@javier.io>
2018-08-14 04:46:29 -0500

    hello-world -> app

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ class Config(object):
     MONGODB_HOST     = os.environ.get('MONGODB_HOST')     or 'mongodb'
     MONGODB_TCP_PORT = os.environ.get('MONGODB_TCP_PORT') or 27017
     MONGODB_TCP_PORT = int(MONGODB_TCP_PORT)
-    MONGODB_DB       = os.environ.get('MONGODB_DB')       or 'hello-world'
-    MONGODB_USER     = os.environ.get('MONGODB_USER')     or 'hello-world'
-    MONGODB_PASSWD   = os.environ.get('MONGODB_PASSWD')   or 'hello-world'
+    MONGODB_DB       = os.environ.get('MONGODB_DB')       or 'app'
+    MONGODB_USER     = os.environ.get('MONGODB_USER')     or 'app'
+    MONGODB_PASSWD   = os.environ.get('MONGODB_PASSWD')   or 'app'
 
     MONGODB_USER     = urllib.parse.quote_plus(MONGODB_USER)
     MONGODB_PASSWD   = urllib.parse.quote_plus(MONGODB_PASSWD)
